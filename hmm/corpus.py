3559ed602ef3e4a6313c3e5a8bae1c518f0bbd09
Maxime Laudrin <maximedelaudrin@gmail.com>
2015-12-04 17:56:57 +0100

    Added corpus wrapper iterators

---
+++ hmm/corpus.py
@@ -0,0 +1,45 @@
+import os
+
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data')
+TIGER_CORPUS = os.path.join(DATA_DIR, 'german_tiger_train.conll')
+TIGER_UNIV_TAGS = os.path.join(DATA_DIR, 'de_tiger.map')
+
+
+class ConllCorpus:
+    def __init__(self, path):
+        self.path = path
+
+    def __iter__(self):
+        with open(self.path, 'r') as corpus:
+            sent = []
+            for l in corpus:
+                line = l.strip()
+                if line:
+                    fields = line.split('\t')
+                    word, tag = fields[1], fields[3]
+                    sent.append((word, tag))
+                else:
+                    yield sent
+                    sent = []
+
+
+class TigerCorpus(ConllCorpus):
+    def __init__(self):
+        super().__init__(TIGER_CORPUS)
+
+
+class UnivTigerCorpus(TigerCorpus):
+    def __init__(self):
+        super().__init__()
+        self.fetch_tags()
+
+    def __iter__(self):
+        for sent in super().__iter__():
+            yield [(w, self.tags[t]) for w, t in sent]
+
+    def fetch_tags(self):
+        self.tags = {}
+        for line in open(TIGER_UNIV_TAGS, 'r'):
+            fine, univ = line.strip().split('\t')
+            self.tags[fine] = univ
